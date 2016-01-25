c69538ba4568e5752cd7a684e133bf01b647eb89
Dominic Canare <dom@makeict.org>
2016-01-24 22:54:27 -0600

    fancy display

--- software/main.py
+++ software/main.py
@@ -4,27 +4,32 @@ import wiringpi2
 import espeak
 import espeak.core as voice
 
-import signal, sys
-import subprocess
+import signal, sys, os
+
+import screentools
 
 inputPins = [ 1, 2, 3, 4, 5 ]
 shutdownPin = 6
 
-def sayGoodbyeAndExit(signal=None, frame=None, shutdown=False):
-	voice.synth("Goodbye!")
-	if shutdown:
-		subprocess.call('poweroff')
-    sys.exit(0)
+def display(msg):
+	screentools.display(msg)
+	voice.synth(msg)
 
+def sayGoodbyeAndExit(signal=None, frame=None, shutdown=False):
+	display("Goodbye!")
+	if shutdown:
+		os.system('poweroff')
+	sys.exit(0)
+	
 signal.signal(signal.SIGTERM, sayGoodbyeAndExit)
 
-wiringpi2.wiringPiSetupPhys()
 
+wiringpi2.wiringPiSetupPhys()
 for pin in inputPins:
 	wiringpi2.pinMode(pin, 0)
 
 pinStates = [ 0 ] * len(inputPins)
-buffer = ""
+buffer = ''
 try:
 	while True:
 		if wiringPi2.digitalRead(shutdownPin) == 1:
@@ -41,13 +46,12 @@ try:
 				
 			# decode it and append it to the buffer
 			buffer += chr(value + ord('a'))
+			display(buffer)
 			
 			# reset pin buffer
 			pinStates = [ 0 ] * len(inputPins)
-			
-		if sum(pinStates) >= len(inputPins):
-			subprocess.call(['toilet', '-f', 'bigmono9', '-F', 'gay', '-t', buffer])
-			voice.synth(buffer)
+		elif sum(pinStates) >= len(inputPins):
+			display(buffer)
 			buffer = ""
 			
 except KeyboardInterrupt:
---
+++ software/screentools.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+import shutil
+import sys, os
+import subprocess
+
+def renderFancy(text, font='mono12', rainbow=True, startLine=0, endLine=None):
+	output = subprocess.check_output(['figlet', '-ctf', font, text]).decode('utf-8')
+	generatedLines = output.split('\n')
+	lines = []
+	for l in generatedLines:
+		if l.strip() != "":
+			lines.append(l)
+			
+	if endLine is None:
+		endLine = len(lines)
+
+	output = "\n".join(lines[startLine:endLine])
+	if rainbow:
+		os.system('echo "%s" | lolcat' % output)
+	else:
+		print(output)
+		
+def display(text):
+	gotoOutputArea()
+	renderFancy(text, 'mono9', endLine=7)
+
+def gotoOutputArea(x=1, y=22):
+	code = "\x1b7\x1b[%d;%df" % (y, x)
+	print(code + '\n' + code, end='')
+
+def renderCentered(text, colorEvery=None):
+	columns = shutil.get_terminal_size().columns
+	textLength = len(text)
+	
+	if colorEvery is not None:
+		output = ''
+		for i in range(textLength):
+			if (i+2) % colorEvery == 0:
+				output += '\x1b[30m\x1b[47m' + text[i] + '\x1b[0m'
+			else:
+				output += text[i]
+		text = output
+	print(' ' * int((columns - textLength) / 2) + text)
+
+
+
+def showIntro():
+	gotoOutputArea(1, 1)
+	print(chr(27) + "[2J")
+	renderFancy('MakeICT', 'mono12', startLine=0, endLine=8)
+	renderFancy(' Binary   Punched   Card   Reader', 'smblock', False, endLine=4)
+	print()
+	
+def showTable():
+	table = []
+	buffer = ''
+	for c in range(26):
+		table.append(bin(c)[2:].zfill(5))
+		buffer += chr(c + ord('a')) + ' '
+		
+	renderCentered(buffer, 4)
+	renderCentered('-----------------------------------------------------')
+	
+	for d in range(5):
+		buffer = ''
+		for c in range(26):
+			buffer += '%s ' %  table[c][d]
+		renderCentered(buffer, 4)
+
+if __name__ == "__main__":
+	import time
+	import espeak
+	import espeak.core as voice
+
+	showIntro()
+	showTable()
+	buff = ''
+	time.sleep(1)
+	for l in 'Dominic':
+		gotoOutputArea()
+		buff += l
+		voice.synth(l)
+		renderFancy(buff, 'mono9')
+		time.sleep(.1)
+	time.sleep(1)
+	voice.synth(buff)
+	time.sleep(1)
+	print('')
+	
